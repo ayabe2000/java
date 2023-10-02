99fb9537d1262c3b8677ea31005279478e365c51
rina ayabe <rina@ngen.co.jp>
2023-10-02 14:39:48 +0900

    java/issue-5

---
+++ SevenFor.java
@@ -0,0 +1,9 @@
+public class SevenFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 7 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
