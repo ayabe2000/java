b09e666813c1fa77406c614c6b9329c69597e727
rina ayabe <rina@ngen.co.jp>
2023-10-04 15:25:34 +0900

    java/issue-22

---
+++ Sample205.java
@@ -0,0 +1,9 @@
+public class Sample205 {
+    public static void main(String[] args){
+        final int NUMBER = 100;
+        final String STRING = "Hoge";
+        System.out.println(NUMBER);
+        System.out.println(STRING);
+        //finalがついた変数は値を変えられない
+    }
+}
